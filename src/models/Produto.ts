cbe7f10c84a04421d312d8e818ede92580223027
Thays Peixoto <thabysilva12@gmail.com>
2025-12-12 13:42:12 -0300

    criado arquivo molde produto

---
+++ src/models/Produto.ts
@@ -0,0 +1,61 @@
+export enum Categoria {
+    Eletronico,
+    Estetica,
+    Perfumaria,
+    Proteina
+}
+
+export default class Produto {
+    private id: string
+    private nome: string
+    private qtdProduto: number
+    private categoria: Categoria
+
+    public constructor(
+        id: string, 
+        nome: string, 
+        qtdProduto: number, 
+        categoria: Categoria) {
+            this.id = id
+            this.nome = nome
+            this.qtdProduto = qtdProduto
+            this.categoria = categoria
+    }
+    
+    public get getId() : string {
+        return this.id
+    }
+
+    public set setId(id: string) {
+        this.id = id
+    }
+    
+    
+    public get getNome() : string {
+        return this.nome
+    }
+    
+    
+    public set setNome(v : string) {
+        this.nome = v;
+    }
+
+    
+    public get getQtdProduto() : number {
+        return this.qtdProduto
+    }
+
+    
+    public set setQtdProduto(quantidadeDoProduto : number) {
+        this.qtdProduto = quantidadeDoProduto;
+    }
+    
+    
+    public get value() : Categoria {
+        return this.categoria
+    }
+    
+    public set setCategoria(v : Categoria) {
+        this.categoria = v;
+    }
+}
